63c1066e64e0be7d097e4b96f5302a52bf7af633
Sarah Hermanns <sahermanns@gmail.com>
2015-02-23 12:05:34 -0800

    person commit 1

---
+++ Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  ClassRoster
+//
+//  Created by Sarah Hermanns on 2/19/15.
+//  Copyright (c) 2015 SASH. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+  
+  let firstName = "Sarah"
+  let lastName = "Hermanns"
+  
+  init(first: String, last: String){
+    self.firstName = first;
+    self.lastName = last;
+  }
+}
