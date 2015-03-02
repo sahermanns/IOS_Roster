6f162b4c798c943fa3f2ea232c5f04b76cf4c606
Sarah Hermanns <sahermanns@gmail.com>
2015-03-01 16:48:20 -0800

    day 4 homework...not funtional yet

--- ViewController.swift
+++ ViewController.swift
@@ -7,36 +7,61 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+  var people = [Person]()
   
   @IBOutlet weak var tableView: UITableView!
-  let names = ["Sarah", "Kara", "Megan", "Caitlin", "Aleesha", "Fran", "Akimi","Becca", "Sean", "Carrie", "Monty", "Greg", "Patrick"]
-
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.title = "Home"
+    self.tableView.backgroundColor = UIColor.greenColor()
     self.tableView.dataSource = self
+    self.tableView.delegate = self
+    
+    var sarah = Person(firstName: "Sarah", lastName: "Hermanns")
+    var kara = Person(firstName: "Kara", lastName: "Fortney")
+    var megan = Person(firstName: "Megan", lastName: "Miller")
+    var caitlin = Person(firstName: "Caitlin", lastName: "Warnock")
+    var aleesha = Person(firstName: "Aleesha", lastName: "Thurber")
+    var fran = Person(firstName: "Fran", lastName: "Skara")
+    var akimi = Person(firstName: "Akimi", lastName: "Hata")
+    var beccah = Person(firstName: "Beccah", lastName: "Jones")
+    var whitney = Person(firstName: "Whitney", lastName: "Clark")
+    
+    self.people.append(sarah)
+    self.people.append(kara)
+    self.people.append(megan)
+    self.people.append(caitlin)
+    self.people.append(aleesha)
+    self.people.append(fran)
+    self.people.append(akimi)
+    self.people.append(beccah)
+    self.people.append(whitney)
+    
   }
   
-  override func viewWillAppear(animated: Bool) {
-    super.viewWillAppear(animated)
-    println("view will appear")
-  }
   
-  override func viewDidAppear(animated: Bool) {
-    super.viewDidAppear(animated)
-    println("view did appear")
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if segue.identifier == "ShowGreen" {
+      println("showing green")
+      let destinationVC = segue.destinationViewController as
+      PersonDetailViewController
+      let indexPath = self.tableView.indexPathForSelectedRow()
+      let person = self.people[indexPath!.row]
+      destinationVC.selectedPerson = person
+    }
   }
-  
   
   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return self.names.count
+    return self.people.count
   }
   
   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
     
-    let name = self.names[indexPath.row]
-    cell.textLabel?.text = name
+    let person = self.people[indexPath.row]
+    cell.textLabel?.text = person.firstName + " " + person.lastName
     
     return cell
   }
--- day3code.swift
+++
@@ -1,9 +0,0 @@
-//
-//  day3code.swift
-//  ClassRoster
-//
-//  Created by Sarah Hermanns on 2/24/15.
-//  Copyright (c) 2015 SASH. All rights reserved.
-//
-
-import Foundation
