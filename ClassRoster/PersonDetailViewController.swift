20918f872975696c4dac31d851d751c3b49460d0
Sarah Hermanns <sahermanns@gmail.com>
2015-03-05 18:06:08 -0800

    assignment 5

--- ClassRoster/PersonDetailViewController.swift
+++ ClassRoster/PersonDetailViewController.swift
@@ -8,13 +8,25 @@
 
 import UIKit
 
-class PersonDetailViewController: UIViewController {
+class PersonDetailViewController: UIViewController, UITextFieldDelegate {
+  
+  @IBOutlet weak var firstNameLabel: UITextField!
+  
+  @IBOutlet weak var lastNameLabel: UITextField!
   
   var selectedPerson = Person(firstName: "Dummy", lastName: "Dummy")
   
   override func viewDidLoad() {
     super.viewDidLoad()
     self.title = self.selectedPerson.firstName + " " + self.selectedPerson.lastName
+    self.firstNameLabel.delegate = self
+    self.lastNameLabel.delegate = self
+    
+  }
+  
+  func textFieldShouldReturn(textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    return true
   }
   
   override func didReceiveMemoryWarning() {
